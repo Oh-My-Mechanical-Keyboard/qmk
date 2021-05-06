cff10e33fbc1cc51bfc47322bc19ee46d22c5cb4
JX <572673807@qq.com>
2021-05-06 14:09:51 +0800

    del libs

--- lib/rgb_state_reader.d
+++
@@ -1,9 +0,0 @@
-.build/obj_rainbow65_pro_v1_default/../../lib/rgb_state_reader.o: \
- keyboards/rainbow65/keymaps/default/../../lib/rgb_state_reader.c \
- keyboards/rainbow65/config.h keyboards/rainbow65/pro_v1/config.h \
- keyboards/rainbow65/board/custom_board.h \
- keyboards/rainbow65/keymaps/default/config.h
-keyboards/rainbow65/config.h:
-keyboards/rainbow65/pro_v1/config.h:
-keyboards/rainbow65/board/custom_board.h:
-keyboards/rainbow65/keymaps/default/config.h:
